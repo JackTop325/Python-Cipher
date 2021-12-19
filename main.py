5af1823f02bae931104b3452ab8a11392242b5a7
Jackie Jiang <72675067+JackTop325@users.noreply.github.com>
2021-12-19 03:05:31 +0000

    Updated code

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def cipher(word:str,key:int)->str:
   for lett in word:
     newLett = ord(lett)
     newLett = ((newLett + key-65)%26+65)
-    print(newLett-65)
+    #print(newLett-65)
     scrambledWord += chr(newLett)
   return scrambledWord
 
@@ -11,14 +11,21 @@ def deCipher(word:str,key:int)->str:
   scrambledWord = ""
   for lett in word:
     newLett = ord(lett)
-    #print(newLett)
     newLett = ((newLett - key-65)%26+65)
-    print(newLett-65)
+    #print(newLett-65)
     scrambledWord += chr(newLett)
   return scrambledWord
 
-x = cipher("TEST",16)
-print(x)
-
-x = deCipher(x,16)
-print(x)
+print("Cipher: ")
+print("Would you like to use a Cipher(1) or Decipher(2): ")
+option = input()
+if(option == "1" or option == "Cipher"):
+    word = input("Please enter the word: ")
+    key = int(input("Please enter the key: "))
+    print(f"The new word is: {cipher(word,key)}")
+elif(option == "2" or option == "Decipher"):
+    word = input("Please enter the word: ")
+    key = int(input("Please enter the key: "))
+    print(f"The new word is: {deCipher(word,key)}")
+else:
+    print("I'm not sure what you entered")
